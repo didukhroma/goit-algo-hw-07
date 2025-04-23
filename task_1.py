e96ef6ea947cd7732287b8d9b0cbb94be672453f
Roman <didukhroma@gmail.com>
2025-04-23 17:04:57 +0200

    task 1 2 3 done

---
+++ task_1.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+
+# Test
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+print(root)
+print(min_value_node(root))
+
