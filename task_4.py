c1107b8b4dc4ca55f0f3368db3661126d85b8039
Roman <didukhroma@gmail.com>
2025-04-23 17:26:17 +0200

    task 4 done

--- task_4.py
+++ task_4.py
@@ -1,69 +1,34 @@
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
+class Comment:
+    def __init__(self, text, author):
+        self.text = text
+        self.author = author
+        self.replies = list()
+        self.is_deleted = False
 
-    def __str__(self, level=0, prefix="Root: "):
-        ret = "\\t" * level + prefix + str(self.val) + "\\n"
-        if self.left:
-            ret += self.left.__str__(level + 1, "L--- ")
-        if self.right:
-            ret += self.right.__str__(level + 1, "R--- ")
-        return ret
+    def add_reply(self,reply):
+        self.replies.append(reply)
 
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    else:
-        if key < root.val:
-            root.left = insert(root.left, key)
-        else:
-            root.right = insert(root.right, key)
-    return root
+    def remove_reply(self):
+        self.is_deleted = True
+        self.text = "Цей коментар було видалено."
 
-def search(root, key):
-    if root is None or root.val == key:
-        return root
-    if key < root.val:
-        return search(root.left, key)
-    return search(root.right, key)
+    def display(self, depth=0):
+        print("\t" * depth + (self.author + ": " if not self.is_deleted else "") + self.text)
+        for reply in self.replies:
+            reply.display(depth + 1)
 
-def min_value_node(node):
-    current = node
-    while current.left:
-        current = current.left
-    return current
-
-def delete(root, key):
-    if not root:
-        return root
-
-    if key < root.val:
-        root.left = delete(root.left, key)
-    elif key > root.val:
-        root.right = delete(root.right, key)
-    else:
-        if not root.left:
-            temp = root.right
-            root = None
-            return temp
-        elif not root.right:
-            temp = root.left
-            root = None
-            return temp
-        root.val = min_value_node(root.right).val
-        root.right = delete(root.right, root.val)
-    return root
 
 # Test
-root = Node(5)
-root = insert(root, 3)
-root = insert(root, 2)
-root = insert(root, 4)
-root = insert(root, 7)
-root = insert(root, 6)
-root = insert(root, 8)
+root_comment = Comment("Яка чудова книга!", "Бодя")
+reply1 = Comment("Книга повне розчарування :(", "Андрій")
+reply2 = Comment("Що в ній чудового?", "Марина")
 
-print(root)
+root_comment.add_reply(reply1)
+root_comment.add_reply(reply2)
 
+reply1_1 = Comment("Не книжка, а перевели купу паперу ні нащо...", "Сергій")
+reply1.add_reply(reply1_1)
+
+reply1.remove_reply()
+
+root_comment.display()
